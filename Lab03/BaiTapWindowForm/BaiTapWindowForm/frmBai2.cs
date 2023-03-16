7c48f3aeec457ded3bbbded83f011c09f9f8f75e
Gonn Nguyen <113317914+longnguyen126203@users.noreply.github.com>
2023-03-16 23:49:51 +0700

    Update Lab 3.1

---
+++ Lab03/BaiTapWindowForm/BaiTapWindowForm/frmBai2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapWindowForm
+{
+    public partial class frmBai2 : Form
+    {
+        public frmBai2()
+        {
+            InitializeComponent();
+        }
+
+        private void frmBai2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbbTenHang_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var stt = cbbTenHang.SelectedIndex;
+            switch (stt)
+            {
+                case 0:
+                    txtDonGia.Text = "100000";
+                    break;
+                case 1:
+                    txtDonGia.Text = "2000000";
+                     break;
+                case 2:
+                    txtDonGia.Text = "150000";
+                    break;
+
+            }
+        }
+
+        private void btnTinhTien_Click(object sender, EventArgs e)
+        {
+            int donGia = int.Parse(txtDonGia.Text);
+            int soLuong = int.Parse(txtSoLuong.Text);
+            double thanhTien = donGia * soLuong;
+
+            if(rdbChuyenKhoan.Checked)
+            {
+                thanhTien = donGia * soLuong * 0.95;
+
+            }
+
+            //Hiển thị kết quả
+            lblSoTienThanhToan.Text = thanhTien.ToString();
+        }
+
+        private void lblSoTien_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
