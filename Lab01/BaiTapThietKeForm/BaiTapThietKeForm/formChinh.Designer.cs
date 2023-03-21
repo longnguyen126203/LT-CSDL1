5b3fd7f08f49f1f97925b8bf317a81e84d54258a
Gonn Nguyen <113317914+longnguyen126203@users.noreply.github.com>
2023-03-21 21:04:50 +0700

    Cap nhat lan 2

---
+++ Lab01/BaiTapThietKeForm/BaiTapThietKeForm/formChinh.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace BaiTapThietKeForm
+{
+    partial class formChinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formChinh));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nguyễnHoàngBảoLongToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmlBai1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmlBai2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmlBai3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiBai4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nguyễnHoàngBảoLongToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nguyễnHoàngBảoLongToolStripMenuItem
+            // 
+            this.nguyễnHoàngBảoLongToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmlBai1,
+            this.tsmlBai2,
+            this.tsmlBai3,
+            this.tsmiBai4});
+            this.nguyễnHoàngBảoLongToolStripMenuItem.Name = "nguyễnHoàngBảoLongToolStripMenuItem";
+            this.nguyễnHoàngBảoLongToolStripMenuItem.Size = new System.Drawing.Size(142, 20);
+            this.nguyễnHoàngBảoLongToolStripMenuItem.Text = "Họ và tên của sinh viên";
+            // 
+            // tsmlBai1
+            // 
+            this.tsmlBai1.Image = ((System.Drawing.Image)(resources.GetObject("tsmlBai1.Image")));
+            this.tsmlBai1.Name = "tsmlBai1";
+            this.tsmlBai1.Size = new System.Drawing.Size(180, 22);
+            this.tsmlBai1.Text = "Bài 1";
+            this.tsmlBai1.Click += new System.EventHandler(this.tsmlBai1_Click);
+            // 
+            // tsmlBai2
+            // 
+            this.tsmlBai2.Image = ((System.Drawing.Image)(resources.GetObject("tsmlBai2.Image")));
+            this.tsmlBai2.Name = "tsmlBai2";
+            this.tsmlBai2.Size = new System.Drawing.Size(180, 22);
+            this.tsmlBai2.Text = "Bài 2";
+            this.tsmlBai2.Click += new System.EventHandler(this.tsmlBai2_Click);
+            // 
+            // tsmlBai3
+            // 
+            this.tsmlBai3.Image = ((System.Drawing.Image)(resources.GetObject("tsmlBai3.Image")));
+            this.tsmlBai3.Name = "tsmlBai3";
+            this.tsmlBai3.Size = new System.Drawing.Size(180, 22);
+            this.tsmlBai3.Text = "Bài 3";
+            this.tsmlBai3.Click += new System.EventHandler(this.tsmlBai3_Click);
+            // 
+            // tsmiBai4
+            // 
+            this.tsmiBai4.Name = "tsmiBai4";
+            this.tsmiBai4.Size = new System.Drawing.Size(180, 22);
+            this.tsmiBai4.Text = "Bài 4";
+            this.tsmiBai4.Click += new System.EventHandler(this.tsmiBai4_Click);
+            // 
+            // formChinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "formChinh";
+            this.Text = "Chương trình chính";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nguyễnHoàngBảoLongToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmlBai1;
+        private System.Windows.Forms.ToolStripMenuItem tsmlBai2;
+        private System.Windows.Forms.ToolStripMenuItem tsmlBai3;
+        private System.Windows.Forms.ToolStripMenuItem tsmiBai4;
+    }
+}
+
---
+++ Lab01/BaiTapThietKeForm/BaiTapThietKeForm/frmBai3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapThietKeForm
+{
+    public partial class frmBai3 : Form
+    {
+        List<string> list = new List<string>();
+        public frmBai3()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var tu = txtTuMoi.Text;
+            var nghia = txtNghiaAnh.Text;
+
+            listBox1.Items.Add(tu);
+            list.Add(nghia);
+
+            txtTuMoi.Focus();
+            txtTuMoi.Text = "";
+            txtNghiaAnh.Text = "";
+
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            txtHienThiNghia.Text = nghia;
+        }
+
+        private void txtHienThiNghia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Lay so thu tu cua tu trong listbox
+            var stt = listBox1.SelectedIndex;   
+            
+            //Tra nghia cua tu trong danh sach list dua vao so thu tu 
+            txtHienThiNghia.Text = list[stt];
+        }
+    }
+}
